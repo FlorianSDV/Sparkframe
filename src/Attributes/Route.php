21b16a0acee59c9df581e50a06ddecd48f160605
FlorianSDV <florian-de-vries@hotmail.com>
2025-04-13 20:07:37 +0200

    Allow there to be multiple attributes

--- src/Attributes/Route.php
+++ src/Attributes/Route.php
@@ -4,7 +4,7 @@ namespace Sparkframe\Attributes;
 
 use Attribute;
 
-#[\Attribute(Attribute::TARGET_METHOD)]
+#[\Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class Route
 {
     public function __construct(private string $route, private string $request_method)
