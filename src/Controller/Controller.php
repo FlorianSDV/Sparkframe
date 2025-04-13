5dc40197d5bdf11fb319313ce0cfe2e48f9eeb0b
florian-de-vries@hotmail.com <93703843+FlorianSDV@users.noreply.github.com>
2025-04-13 17:00:54 +0200

    implement request handling

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,6 +2,7 @@
 
 namespace Sparkframe\Controller;
 
+use Sparkframe\Attributes\Route;
 use Sparkframe\Model\Model;
 
 abstract class Controller
@@ -19,5 +20,41 @@ abstract class Controller
     {
         //todo: een controller heeft toegang nodig tot de request.
         // Dus de request moet al bestaan in de globals.
+    }
+
+
+    /**
+     * Method to check if a class is a controller
+     * @return bool
+     */
+    public function controllerMethod(): bool
+    {
+        return true;
+    }
+
+    public function getRoutes(): array
+    {
+        $controller_routes = [];
+        $reflection = new \ReflectionClass(static::class);
+        $methods = $reflection->getMethods();
+        foreach ($methods as $method) {
+            $method_routes = $method->getAttributes(name: Route::class);
+            if (count($method_routes) === 0) {
+                continue;
+            }
+
+            foreach ($method_routes as $method_route) {
+                /**
+                 * @var Route $new_method_route_instance
+                 */
+                $new_method_route_instance = $method_route->newInstance();
+                $controller_routes[$new_method_route_instance->getRequestMethod()][$new_method_route_instance->getRoute()] = [
+                    'controller' => $method->class,
+                    'method' => $method->name
+                ];
+            }
+        }
+
+        return $controller_routes;
     }
 }
