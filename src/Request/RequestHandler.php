5c27fb167fcfc6bcabf27f895d0a354fe72254d8
FlorianSDV <florian-de-vries@hotmail.com>
2025-04-19 22:21:37 +0200

    Add route properties and variables

--- src/Request/RequestHandler.php
+++ src/Request/RequestHandler.php
@@ -19,11 +19,15 @@ class RequestHandler
      */
     public function handle()
     {
+        // Get the method belonging to this route.
         $method_route = Router::routeToMethod($this->request);
-        
-        $controller = Globals::getController($method_route->getController());
 
+        // Fetch the correct controller, method name and variables from the uri.
+        $controller = Globals::getController($method_route->getController());
         $method = $method_route->getMethodName();
-        return $controller->$method();
+        $variables = $method_route->getVariables();
+
+        // Invoke the method with the correct variables.
+        return $controller->$method(...$variables);
     }
 }
--- src/Tools/MethodRoute.php
+++ src/Tools/MethodRoute.php
@@ -5,9 +5,15 @@ namespace Sparkframe\Tools;
 class MethodRoute
 {
     /**
-     * @var string[] 
+     * @var string[]
      */
     private array $uri;
+
+    /**
+     * @var int|string[]
+     */
+    private array $variables = [];
+
     public function __construct(
         string                  $uri,
         private readonly string $controller,
@@ -27,15 +33,62 @@ class MethodRoute
         return $this->method_name;
     }
 
+    public function getVariables(): array
+    {
+        return $this->variables;
+    }
+
     /**
      * @param string[] $incoming_request_uri
      * @return bool
      */
     public function matchUri(array $incoming_request_uri): bool
     {
-        if ($this->uri === $incoming_request_uri){
+        if ($this->uri === $incoming_request_uri) {
             return true;
         }
+
+        $this_uri_count = count($this->uri) - 1;
+        $incoming_request_uri_count = count($incoming_request_uri) - 1;
+        if ($this_uri_count > $incoming_request_uri_count) {
+            return false;
+        }
+
+        $this->variables = [];
+        for ($i = 0; $i <= $this_uri_count; $i++) {
+            $this_uri_step = $this->uri[$i];
+            if ($this_uri_step === WILDCARD_ROUTE_PROPERTY) {
+                return true;
+            }
+
+            $at_last_step = $this_uri_count === $i && $incoming_request_uri_count === $i;
+            $incoming_request_uri_step = $incoming_request_uri[$i];
+
+            if ($this_uri_step === INT_ROUTE_PROPERTY && is_numeric($incoming_request_uri_step)) {
+                $this->variables[] = intval($incoming_request_uri_step);
+                if ($at_last_step) {
+                    return true;
+                }
+                continue;
+            }
+
+            if ($this_uri_step === STR_ROUTE_PROPERTY) {
+                $this->variables[] = $incoming_request_uri_step;
+                if ($at_last_step) {
+                    return true;
+                }
+                continue;
+            }
+
+            if ($this_uri_step !== $incoming_request_uri_step) {
+                return false;
+            }
+
+            if ($at_last_step) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
