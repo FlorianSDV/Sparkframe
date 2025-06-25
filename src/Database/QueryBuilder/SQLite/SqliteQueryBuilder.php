82a2f967db6d2ea73d2f4da7ff0a6e4d9368b931
florian-de-vries@hotmail.com <93703843+FlorianSDV@users.noreply.github.com>
2025-06-25 17:38:44 +0200

    Implement where conditions and prepared statements

--- src/Database/QueryBuilder/SQLite/SqliteQueryBuilder.php
+++ src/Database/QueryBuilder/SQLite/SqliteQueryBuilder.php
@@ -2,15 +2,24 @@
 
 namespace Sparkframe\Database\QueryBuilder\SQLite;
 
+use Exception;
 use Sparkframe\Database\QueryBuilder\QueryBuilder;
 
 abstract class SqliteQueryBuilder extends QueryBuilder
 {
-    // TODO: refactor
-    public function where(string ...$column_names): SqliteQueryBuilder
+    /**
+     * @throws Exception
+     */
+    public function where(array $filter_criteria): SqliteQueryBuilder
     {
-        foreach ($column_names as $column_name) {
-            $this->where_columns[] = $column_name;
+        foreach ($filter_criteria as $column => $filter_criterion) {
+            if (!is_string($column)) {
+                throw new Exception('Column name must be a string!');
+            }
+            $this->where_conditions[] = [
+                'column' => $column,
+                'filter_criterion' => $filter_criterion
+            ];
         }
 
         return $this;
@@ -20,13 +29,38 @@ abstract class SqliteQueryBuilder extends QueryBuilder
     {
         return "from $this->from_table_name ";
     }
-    public function getWherePart(): string
+
+    public function getPreparedWherePart(): string
     {
-        $where_part = '';
-        foreach ($this->where_columns as $where_column) {
-            // todo: dit pak ik op bij de single select
-            $where_part .= ' ';
+        if (count($this->where_conditions) == 0) {
+            return '';
         }
-        return '';
+
+        $where_part = 'where';
+        foreach ($this->where_conditions as $key => $where_condition) {
+            $where_part .= " $where_condition[column] = :$where_condition[column]";
+
+            if (array_key_last($this->where_conditions) == $key) {
+                break;
+            }
+
+            $where_part .= ' and';
+        }
+
+        return $where_part;
+    }
+
+    public function getPreparedWherePartStatements(): array
+    {
+        if (count($this->where_conditions) == 0) {
+            return [];
+        }
+
+        $prepared_statements = [];
+        foreach ($this->where_conditions as $where_condition) {
+            $prepared_statements[$where_condition['column']] = $where_condition['filter_criterion'];
+        }
+
+        return $prepared_statements;
     }
 }
