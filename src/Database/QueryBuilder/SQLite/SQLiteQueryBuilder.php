ec8003476a06c7ae4645498257ab70dd4ef67ed1
FlorianSDV <florian-de-vries@hotmail.com>
2025-08-06 13:12:53 +0200

    Change method name for retrieving table name.

---
+++ src/Database/QueryBuilder/SQLite/SQLiteQueryBuilder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sparkframe\Database\QueryBuilder\SQLite;
+
+use Exception;
+use Sparkframe\Database\QueryBuilder\QueryBuilder;
+
+abstract class SQLiteQueryBuilder extends QueryBuilder
+{
+    public function getTargetTable(): string
+    {;
+        return $this->target_table_name;
+    }
+}
