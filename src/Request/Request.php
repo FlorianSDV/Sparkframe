2261c5472a16e4c7160b99d476617b630a1b172a
FlorianSDV <florian-de-vries@hotmail.com>
2025-08-06 17:50:11 +0200

    Add request body to Request class.

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -6,10 +6,12 @@ class Request
 {
     private string $uri;
     private string $request_method;
+    private string $request_body;
     public function __construct()
     {
         $this->uri = $_SERVER['REQUEST_URI'];
         $this->request_method = $_SERVER['REQUEST_METHOD'];
+        $this->request_body = file_get_contents('php://input');
     }
 
     public function getUri(): string
@@ -20,5 +22,10 @@ class Request
     public function getRequestMethod(): string
     {
         return $this->request_method;
+    }
+
+    public function getRequestBody(): string
+    {
+        return $this->request_body;
     }
 }
