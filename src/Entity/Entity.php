441ad98410f1599670af50f3b1e7fed3235629dd
florian-de-vries@hotmail.com <93703843+FlorianSDV@users.noreply.github.com>
2025-08-06 13:14:32 +0200

    Create insert functionality.

--- src/Entity/Entity.php
+++ src/Entity/Entity.php
@@ -2,7 +2,27 @@
 
 namespace Sparkframe\Entity;
 
-class Entity
+abstract class Entity
 {
+    protected const array COLUMN_DESCRIPTIONS = [];
 
+    public static function getColumnNames(): array
+    {
+        return array_keys(static::getColumnDescriptions());
+    }
+
+    public static function getColumnDescriptions(): array
+    {
+        return self::COLUMN_DESCRIPTIONS;
+    }
+
+    public function setId(string|int $id): void
+    {
+        foreach ($this::getColumnDescriptions() as $key => $column) {
+            if (is_array($column) && in_array('primary', $column)) {
+                $this->$key = $id;
+                break;
+            }
+        }
+    }
 }
