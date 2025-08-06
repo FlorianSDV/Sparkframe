79c228657b968b4d15f0319deb1208988bd74a45
FlorianSDV <florian-de-vries@hotmail.com>
2025-08-06 17:49:44 +0200

    Add functionality for inserting multiple entities

--- src/Database/DataBaseConnection.php
+++ src/Database/DataBaseConnection.php
@@ -2,12 +2,14 @@
 
 namespace Sparkframe\Database;
 
+use PDO;
 use Sparkframe\Database\QueryBuilder\SelectQueryBuilder;
 use Sparkframe\Database\QueryBuilder\InsertQueryBuilder;
 
 interface DataBaseConnection
 {
+    public function getPdo(): PDO;
     //todo: implement various methods for building queries
     public function selectQuery(string $from_table_name): SelectQueryBuilder;
-    public function insertQuery(string $insert_into_table_name): InsertQueryBuilder;
+    public function insertQuery(string $insert_into_table_name, string $entity_class): InsertQueryBuilder;
 }
