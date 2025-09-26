4dd039f42fc4c3450f755b05eb329b85c9f9e403
FlorianSDV <florian-de-vries@hotmail.com>
2025-09-26 23:30:50 +0200

    Add delete functionality

--- src/Database/DatabaseWrapper.php
+++ src/Database/DatabaseWrapper.php
@@ -3,6 +3,7 @@
 namespace Sparkframe\Database;
 
 use PDO;
+use Sparkframe\Database\QueryBuilder\DeleteQueryBuilder;
 use Sparkframe\Database\QueryBuilder\SelectQueryBuilder;
 use Sparkframe\Database\QueryBuilder\InsertQueryBuilder;
 use Sparkframe\Database\QueryBuilder\UpdateQueryBuilder;
@@ -19,4 +20,5 @@ interface DatabaseWrapper
     public function selectQuery(string $from_table_name): SelectQueryBuilder;
     public function insertQuery(string $insert_into_table_name, string $entity_class): InsertQueryBuilder;
     public function updateQuery(string $update_table_name, string $entity_class): UpdateQueryBuilder;
+    public function deleteQuery(string $delete_from_table_name, string $entity_class): DeleteQueryBuilder;
 }
