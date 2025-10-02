458ad44faa635e6d786f1b08ccc376e12c3000a3
florian-de-vries@hotmail.com <93703843+FlorianSDV@users.noreply.github.com>
2025-10-02 23:26:24 +0200

    Add post data to request

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -9,11 +9,14 @@ class Request
     private string $uri;
     private string $request_method;
     private string $request_body;
+    private array $request_post;
     public function __construct()
     {
+        session_start();
         $this->uri = $_SERVER['REQUEST_URI'];
         $this->request_method = $_SERVER['REQUEST_METHOD'];
         $this->request_body = file_get_contents('php://input');
+        $this->request_post = $_POST;
     }
 
     public function getUri(): string
@@ -29,5 +32,10 @@ class Request
     public function getRequestBody(): string
     {
         return $this->request_body;
+    }
+
+    public function getRequestPost(): array
+    {
+        return $this->request_post;
     }
 }
