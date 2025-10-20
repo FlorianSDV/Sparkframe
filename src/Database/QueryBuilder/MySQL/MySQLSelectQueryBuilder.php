48a83e1566ac6dc7bd35f4d058f7cefa8e5c9097
Florian Riecker- de Vries <93703843+FlorianSDV@users.noreply.github.com>
2025-10-20 16:23:07 +0200

    Fix generation of mysql query with or (#13)

--- src/Database/QueryBuilder/MySQL/MySQLSelectQueryBuilder.php
+++ src/Database/QueryBuilder/MySQL/MySQLSelectQueryBuilder.php
@@ -64,7 +64,7 @@ class MySQLSelectQueryBuilder implements SelectQueryBuilderInterface
             if (!is_string($expression)) {
                 throw new Exception('Expression must be a string!');
             }
-            $conditions = [
+            $conditions[] = [
                 'expression' => $expression,
                 'filter_criterion' => $filter_criterion
             ];
@@ -224,7 +224,7 @@ class MySQLSelectQueryBuilder implements SelectQueryBuilderInterface
         $or_part = 'or ';
         foreach ($this->or_conditions as &$or_condition_array) {
             $temp_or_array = [];
-            foreach ($or_condition_array as $or_condition) {
+            foreach ($or_condition_array as &$or_condition) {
                 $temp_or_array[] = $or_condition['expression'] . ' :' . $this->prepared_statement_index;
                 $or_condition['prepared_statement_index'] = $this->prepared_statement_index;
                 $this->prepared_statement_index++;
@@ -261,9 +261,11 @@ class MySQLSelectQueryBuilder implements SelectQueryBuilderInterface
             return [];
         }
         $prepared_statements = [];
-        foreach ($this->or_conditions as $or_condition) {
-            $parameter_name = ':' . $or_condition['prepared_statement_index'];
-            $prepared_statements[$parameter_name] = $or_condition['filter_criterion'];
+        foreach ($this->or_conditions as $or_condition_array) {
+            foreach ($or_condition_array as $or_condition) {
+                $parameter_name = ':' . $or_condition['prepared_statement_index'];
+                $prepared_statements[$parameter_name] = $or_condition['filter_criterion'];
+            }
         }
 
         foreach ($this->or_in_conditions as $or_in_condition_array) {
