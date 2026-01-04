c54b7f4b633616af262887d0d8a2ed175f0ed08c
Florian Riecker- de Vries <florian-de-vries@hotmail.com>
2026-01-04 18:30:36 +0100

    Switch to PDO SQLite and use setup function

--- tests/Database/QueryBuilder/SQLite/SQLiteInsertQueryBuilderTest.php
+++ tests/Database/QueryBuilder/SQLite/SQLiteInsertQueryBuilderTest.php
@@ -3,19 +3,26 @@ declare(strict_types=1);
 
 namespace Sparkframe\Tests\Database\QueryBuilder\SQLite;
 
-use PDO;
+use Pdo\Sqlite;
 use PHPUnit\Framework\TestCase;
-use Sparkframe\Database\QueryBuilder\SQLite\SQLiteInsertQueryBuilder;
+use Sparkframe\Database\SqliteDatabaseWrapper;
 use Sparkframe\Tests\Mocks\Entities\MockEntity;
 
 
 class SQLiteInsertQueryBuilderTest extends TestCase
 {
+    private function createSqliteDatabaseWrapper(): SqliteDatabaseWrapper
+    {
+        $sqlite_mock = $this->createMock(SQLite::class);
+        $sqlite_database_wrapper = new SqliteDatabaseWrapper($sqlite_mock);
+        return $sqlite_database_wrapper;
+    }
+
     public function testInsertQuery()
     {
-        $pdo = $this->createMock(PDO::class);
+        $sqlite_database_wrapper = $this->createSqliteDatabaseWrapper();
 
-        $insertQueryBuilder = new SQLiteInsertQueryBuilder($pdo, 'users', MockEntity::class);
+        $insertQueryBuilder = $sqlite_database_wrapper->insertQuery('users', MockEntity::class);
         $reflectionMethod = new \ReflectionMethod($insertQueryBuilder, 'getQuery');
 
         $columns = MockEntity::getColumnNames();
